a1da7ebb42b2752d65fe03346360d078c5b4763f
Souji <sduggirala3@gatech.edu>
2019-10-31 13:54:39 -0400

    Update colortester.py

--- GUI/colortester.py
+++ GUI/colortester.py
@@ -11,17 +11,22 @@ df= pd.read_csv('oct_2019 MELT.csv')
 
 
 #desk filter
-def desk_filter(day,desk):
-    global df2
-    desk_new=[]
-    for i in desk:
-        desk_new.append(str(i))
+def desk_filter(df, date, desk):
+    '''
+    Def: Filters desk by desk names and day
+    Inputs:
+        df: full melted dataframe
+        day: datetime element YYYY-MM-DD (as strings)
+        desk: list of desks to pull (as strings)
+    Return: new dataframe with filtered flights by desk and date
+    '''
 
-    df1= df[df.Day.isin(day)]
-    df2=df1[df1.Desk.isin(desk_new)]  #needs to be a string
-    return (df2)
+    df_date = df.loc[df['Date'] == date]
+    df_date_desk = df_date[df_date['Desk'].isin(desk)]
 
-desk_filter([1],[3])
+    return df_date_desk
+
+desk_filter(df, [1],[3])
 #print(df2)
 index = 0
 
